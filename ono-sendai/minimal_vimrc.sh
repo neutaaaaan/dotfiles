e5b7d33f842ffdba36ca467f8f2e15ad6d614597
neutaaaaan <neutaaaaan-gh@protonmail.com>
2022-08-18 15:12:17 +0200

    replace ansi colors with xterm colors

--- ono-sendai/minimal_vimrc.sh
+++ ono-sendai/minimal_vimrc.sh
@@ -30,6 +30,7 @@ set t_Co=256
 set background=dark
 augroup highlight_override
 	autocmd!
-	autocmd ColorScheme * hi String ctermfg=4
+	autocmd ColorScheme * hi Special ctermfg=161
+	autocmd ColorScheme * hi String ctermfg=32
 augroup END
 colorscheme quiet" > "$HOME"/.vimrc
